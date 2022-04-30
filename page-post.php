e074e530cbe5349bac8a56fae97c592dafb531eb
mochimochi628 <66673086+mochimochi628@users.noreply.github.com>
2022-04-30 11:21:16 +0900

    Create page-post.php

---
+++ page-post.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<div class="mypostlist">
+	<?php if(have_posts()): while(have_posts()): the_post(); ?>
+
+		<article <?php post_class(); ?>>
+			<a href="<?php the_permalink() ;?>">
+			<?php if(has_post_thumbnail() ): ?>
+				<figure>
+					<?php the_post_thumbnail(); ?>
+				</figure>
+			<?php endif; ?>
+
+			<h2><?php the_title(); ?></h2>
+			</a>
+		</article>
+
+	<?php endwhile; endif; ?>
+</div>
+
+<?php get_sidebar(); ?>
+	  
+<?php get_footer(); ?>
