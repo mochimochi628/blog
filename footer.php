deb56658268848be9394edb130ee63911f392807
mochimochi628 <66673086+mochimochi628@users.noreply.github.com>
2022-05-12 17:07:43 +0900

    Update footer.php
    フッターにナビゲーションメニュー、コピーライト追加

--- footer.php
+++ footer.php
@@ -1,5 +1,25 @@
 <footer class="myfoot">
-	  <?php bloginfo('name'); ?>
+	<div class=alignwide>
+	    <div class="footer-menu">
+		  <ul class="footer-menu">
+			<li><a href="https://mochimochi628.com/" aria-current="page">ホーム</a></li>
+			<li><a href="https://mochimochi628.com/about/">サイトについて</a></li>
+			<li><a href="https://mochimochi628.com/profile/">プロフィール</a></li>
+			<li><a href="https://mochimochi628.com/privacy-policy/">プライバシーポリシー</a></li>
+			<li><a href="https://mochimochi628.com/disclaimer/">免責事項</a></li>
+			<li><a href="https://mochimochi628.com/contact/">お問い合わせ</a></li>
+		  </ul>	
+		</div>
+		
+		<div class="copyright">
+			<p>
+				Copyright © 2021 もちもちぐらし Inc. All Rights Reserved.
+			</p>
+		</div>
+	</div>
+	
+	</div>
+	  
 	</footer>
 	  
 	<?php wp_footer(); ?>
