e145ce4cc66f9ce484d953f646f337ddb87f3cfa
mochimochi628 <66673086+mochimochi628@users.noreply.github.com>
2022-05-13 11:02:30 +0900

    Update front-page.php
    コメント追加、もしもアフィリエイトの広告バナー削除

--- front-page.php
+++ front-page.php
@@ -1,3 +1,9 @@
+<?php
+/*
+トップページ表示
+*/
+?>
+
 <?php get_header(); ?>
 
 <div class="mypostlist">
@@ -20,8 +26,6 @@
 
 	<?php endwhile; endif; ?>
 </div>
-
-<a href="//af.moshimo.com/af/c/click?a_id=3292872&p_id=2688&pc_id=6039&pl_id=47679&url=https%3A%2F%2Fhomeo-jp.net%2Ffreeband.html" rel="nofollow" referrerpolicy="no-referrer-when-downgrade"><img src="https://image.moshimo.com/af-img/2121/000000047679.jpg" style="border:none;"></a><img src="//i.moshimo.com/af/i/impression?a_id=3292872&p_id=2688&pc_id=6039&pl_id=47679" width="1" height="1" style="border:none;">
 
 <?php the_posts_pagination( array(
     'prev_text' => '&lt;',
